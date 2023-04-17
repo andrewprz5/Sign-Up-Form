5d43c60cf9cf1e53c78bf615f6a07fe88e1e8886
andrewperez <andrewprz5@gmail.com>
2023-04-16 20:48:01 -0400

    Project Edit

--- index.js
+++ index.js
@@ -16,10 +16,10 @@ const match = () => {
     para.style.width = "14rem";
     const text = document.createTextNode("*Passwords do not match.");
     const errorDiv = document.getElementById("error");
-    const form = document.getElementById("form");
     const pwdCss = document.querySelector("input#pwd");
     const cpwdCss = document.querySelector("input#c_pwd");
     if ((pwdText != confText) && (numAttempts == 0)) {
+        document.querySelector("form").setAttribute("onsubmit", "event.preventDefault();");
         para.appendChild(text);
         errorDiv.appendChild(para);
         numAttempts = 1;
@@ -27,14 +27,10 @@ const match = () => {
         confText.value = "";
         pwdCss.style.border = "1px solid red";
         cpwdCss.style.border = "1px solid red";
-    } else {
-        form.removeAttribute("onsubmit", "event.preventDefault();");
+    } else if ((pwdText != confText)) {
+        document.querySelector("form").setAttribute("onsubmit", "event.preventDefault();");
     }
+    document.querySelector("form").removeAttribute("onsubmit", "event.preventDefault();");
 };
 
 
-
-// need to prevent form submission when passwords don't match and 
-// only allow error msg output occur once, in the case of inputting
-// multiple failed password attempts, and allow submission when pwds
-// do match
